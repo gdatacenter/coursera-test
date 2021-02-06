caddcc390baef0473775890e8c4136b27bde0282
jason25 <lsancehz@valvps.com>
2021-02-06 16:40:53 -0500

    web

---
+++ site/header.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="keywords" content="HTML5 Template">
+<meta name="description" content="Closter WHMCS Web Hosting Template">
+<meta name="author" content="rctheme.com">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<title>Closter WHMCS Web Hosting Template</title>
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="images/favicon.ico">
+
+<!-- Google Fonts -->
+<link rel="stylesheet" type="text/css" href="../../css-1.css?family=Quicksand:300,400,500,600,700&display=swap">
+
+<!-- Bootstrap -->
+<link rel="stylesheet" href="css/bootstrap.css">
+
+<!-- Font Awesome -->
+<link rel="stylesheet" href="css/font-awesome.css">
+
+<!-- Style -->
+<link rel="stylesheet" href="css/style.css">
+
+<!-- Responsive -->
+<link rel="stylesheet" href="css/responsive.css">
+
+<!-- custom style -->
+<link rel="stylesheet" href="css/color/default.css">
+</head>
+
+<body>
+<div class="top-header dark-bg">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+        <ul class="left-top-bar">
+          <li><i class="fa fa-phone" aria-hidden="true"></i> <a href="tel:+784596321">+784596321</a></li>
+          <li><i class="fa fa-envelope-o" aria-hidden="true"></i> <a href="info@rctheme.com">info@rctheme.com</a></li>
+        </ul>
+      </div>
+      <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+        <ul class="right-top-bar">
+          <li><a href="#">Login</a></li>
+          <li><a href="#">Support</a></li>
+          <li>
+            <select>
+              <option>Select Language</option>
+              <option>English</option>
+              <option>Hindi</option>
+            </select>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<header class="transparent-header">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-3 col-md-6">
+        <div class="logo"> <a href="index.html"><img src="images/logo.png" alt="Logo"></a> </div>
+      </div>
+      <div class="col-lg-9 order-12 order-lg-0">
+        <div class="menu-area">
+          <div class="main-menu text-right">
+            <nav id="mobile-menu">
+              <ul>
+                <li class="active"><a href="index.html">Home</a></li>
+                <li class="menu-item-has-children"><a href="#">Domains & Hosting </a>
+                  <ul class="submenu">
+                    <li><a href="domain-search.html">Domain Search</a></li>
+                    <li><a href="transfer-domain.html">Transfer Domain</a></li>
+                    <li><a href="shared-hosting.html">Shared Hosting</a></li>
+                    <li><a href="reseller-hosting.html">Reseller Hosting</a></li>
+                    <li><a href="wordpress-hosting.html">WordPress Hosting</a></li>
+                    <li><a href="cloud-hosting.html">Cloud Hosting</a></li>
+                  </ul>
+                </li>
+                <li class="menu-item-has-children"><a href="#">Servers </a>
+                  <ul class="submenu">
+                    <li><a href="dedicated-server.html">Dedicated Server</a></li>
+                    <li><a href="vps.html">Virtual Private Server </a></li>
+                  </ul>
+                </li>
+                <li class="menu-item-has-children"><a href="#">Email & Security </a>
+                  <ul class="submenu">
+                    <li><a href="ssl-certificates.html">SSL Certificates </a></li>
+                    <li><a href="sitelock.html">SiteLock</a></li>
+                    
+                    <li><a href="business-email.html">Business Email</a></li>
+                    <li><a href="google-apps.html">Google Apps</a></li>
+                  </ul>
+                </li>
+                <li><a href="about-us.html">About us</a></li>
+                <li><a href="contact-us.html">Contact</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="mobile-menu"></div>
+      </div>
+    </div>
+  </div>
+</header>
+<div class="home-banner">
+  <div class="container">
+    <div class="row">
+      <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 relative">
+        <h1>Web Hosting made Easy and Affordable</h1>
+        <p class="w-75 m-auto">Sollicitudin ac orci phasellus egestas. Urna nunc id cursus metus aliquam eleifend. Neque vitae tempus Sollicitudin  id cursus metus aliquam eleifend. Neque vitae tempus Sollicitudinac orci phasellus egestas Urna nunc</p>
+        <a href="#">Get Start</a> </div>
+    </div>
+  </div>
+</div>
